92310b41e0c6721cbea245583a76fe5c455c10f1
Nick Dean <klick5000@gmail.com>
2018-12-07 21:53:31 -0700

    changed db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/restaurants-app";
+  process.env.DATABASE_URL || "mongodb://localhost/thinkful-test-db";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-restaurants-app";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/thinkful-test-db";
 exports.PORT = process.env.PORT || 8080;
